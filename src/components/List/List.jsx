c952ad8e36b5713bc49e5e50813978971a849698
Gifftybabe <ulokangozi@gmail.com>
2023-11-23 18:34:19 +0100

    Fetching data for restaurants

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -9,12 +9,26 @@ import {
   Select,
 } from "@material-ui/core";
 
+import PlaceDetails from "../PlaceDetails/PlaceDetails";
+
 import useStyles from "./styles";
 
 const List = () => {
   const classes = useStyles();
   const [type, setType] = useState("restaurants");
   const [rating, setRating] = useState("");
+
+  const places = [
+    { name: "Cool Place" },
+    { name: "Best Beer" },
+    { name: "Best steak" },
+    { name: "Cool Place" },
+    { name: "Best Beer" },
+    { name: "Best steak" },
+    { name: "Cool Place" },
+    { name: "Best Beer" },
+    { name: "Best steak" },
+  ];
 
   return (
     <div className={classes.container}>
@@ -39,6 +53,14 @@ const List = () => {
           <MenuItem value={4.5}>Above 4.5</MenuItem>
         </Select>
       </FormControl>
+
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, i) => (
+          <Grid item key={i} xs={12}>
+            <PlaceDetails place={place} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
