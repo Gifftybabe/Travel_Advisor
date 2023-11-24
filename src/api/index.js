637df36d6e64ec201fbda7bb7fc1c6ce536e8a0f
Gifftybabe <ulokangozi@gmail.com>
2023-11-24 18:16:04 +0100

    type and rating card uploaded

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,25 @@
 import axios from "axios";
 
-const URL =
-  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
-
-export const getPlacesData = async (sw, ne) => {
+export const getPlacesData = async (type, sw, ne) => {
   try {
     const {
       data: { data },
-    } = await axios.get(URL, {
-      params: {
-        bl_latitude: sw.lat,
-        tr_latitude: ne.lat,
-        bl_longitude: sw.lng,
-        tr_longitude: ne.lng,
-      },
-      headers: {
-        "X-RapidAPI-Key": "40811a1e6amsh4b504c15df2faa7p149c55jsn6e49a3514868",
-        "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
-      },
-    });
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          "X-RapidAPI-Key":
+            "40811a1e6amsh4b504c15df2faa7p149c55jsn6e49a3514868",
+          "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+        },
+      }
+    );
 
     return data;
   } catch (error) {
